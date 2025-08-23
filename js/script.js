d0c5bc7ada101a03ee1fb42cf87c00c5eaeda7af
PRINCE MAKHANSA <princemakhansa@gmail.com>
2025-08-23 19:51:22 +0530

    navbar animation

--- js/script.js
+++ js/script.js
@@ -4,4 +4,26 @@
       }, 1950); 
     });
 
+document.addEventListener("DOMContentLoaded", () => {
+
+  const navbar = document.querySelector(".navbar");
+ 
+  let prevScroll = window.scrollY;
+  const navHeight = navbar.offsetHeight;
+
+  window.addEventListener("scroll", () => {
+
+    let currentScroll = window.scrollY;
+
+    if (currentScroll > prevScroll) {
+      // scrolling down → hide
+      navbar.style.top = `-${navHeight}px`;
+    } else {
+      // scrolling up → show
+      navbar.style.top = "0";
+    }
+
+    prevScroll = currentScroll;
+  });
+});
 
